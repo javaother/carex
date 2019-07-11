efe206f447ddb971fe9fecaf44c12934b475798c
Shepico <Shepico@gmail.com>
2019-07-11 10:56:13 +0400

    new project

---
+++ src/main/java/ru/shepico/carex/CarexApplication.java
@@ -0,0 +1,14 @@
+package ru.shepico.carex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarexApplication.class, args);
+	}
+
+}
+
